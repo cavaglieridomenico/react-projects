114a78d8be2ad2ec0aaa2e22a6fae4287fd9a4ae
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-23 16:19:58 +0200

    [UPDATED] birthday-reminder Components
    Completed the Components App and List of the first React project.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [peaple, setPeaple] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{peaple.length} birthday today</h3>
+        <List peaple={peaple}></List>
+        <button onClick={() => setPeaple([])}>Clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
