da408dfa90803028fdf5a6d2e98f9aff2e3b8af6
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-25 12:57:34 +0200

    [UPDATED] reviews: used for list index

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,7 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
 };
 
 export default Review;
