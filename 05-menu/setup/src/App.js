ff41e9f32563e4b22092b667de5e951e8ef4144e
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-27 21:02:36 +0200

    [ADDED] Menu: Buttons categories elements in App
    In Component App added dynamically, the
    Buttons categories Components with the function to filter menu items by
    category.

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,12 +4,34 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+
+  const categories = () => {
+    const uniqueCategory = ['all', ...new Set(items.map(el => el.category))];
+    return uniqueCategory;
+  };
+
+  const filterItems = category => {
+    setMenuItems(prevItem => {
+      if (category === 'all') {
+        return items;
+      }
+      const newItems = items.filter(el => el.category === category);
+      return newItems;
+    });
+  };
+
   return (
     <main>
       <section className='menu section'>
         <div className='title'>
           <h2>our menu</h2>
           <div className='underline'></div>
+        </div>
+        <div className='btn-container'>
+          {categories().map(el => (
+            <Categories key={el} category={el} filterItems={filterItems} />
+          ))}
         </div>
         <div className='section-center'>
           {menuItems.map(el => {
