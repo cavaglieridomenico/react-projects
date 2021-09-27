2b14040c956973ef1ff95aca9bfbeaca99c9ccd0
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-27 20:53:45 +0200

    [ADDED] Menu: Menu elements in App
    In Component App added the static elements and, dynamically, the
    Components Menu.

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,21 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <div className='section-center'>
+          {menuItems.map(el => {
+            return <Menu key={el.id} {...el} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
