96e8474bf8b0aae7ce5aa86b49298cb755bef4f9
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-02 17:51:07 +0200

    [ADDED] Logic for managing the indexes of the list
    Added the logic and the useState for managing the indexes of the list of
    elements that will be part of the slideshow.

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,23 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const startNumberOfList = 0;
+  const lastNumberOfList = data.length - 1;
+  const [currentIndex, setCurrentIndex] = useState(startNumberOfList);
+
+  const checkIndex = number => {
+    if (number > lastNumberOfList) {
+      return (number = startNumberOfList);
+    }
+    if (number < startNumberOfList) {
+      return (number = lastNumberOfList);
+    }
+    return number;
+  };
+
+  useEffect(() => {
+    setCurrentIndex(checkIndex(currentIndex));
+  }, [currentIndex]);
 }
 
 export default App;
