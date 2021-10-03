4ba96a37f390f810cca010c31e375d1a0e6528b8
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-03 08:52:26 +0200

    [ADDED] Structure of the Component App

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -4,8 +4,25 @@ function App() {
   
   
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form'>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          id='amount'
+        />
+        <button
+          className='btn'
+          type='submit'
+        >
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+      </article>
+    </section>
+  );
 }
 
 export default App;
