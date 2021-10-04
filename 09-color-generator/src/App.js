d87b7e96a87ddee73709126ca8de6ab1d81f7905
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-04 16:21:34 +0200

    [ADDED] Logic for using the Values.js library
    The function accepts a string related to a color and returns an array
    with the required tints and shades. It has a try catch logic to validate
    the inserted data and possibly use the throw of the library to throw the
    error.

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import SingleColor from './SingleColor';
 import Values from 'values.js';
+
 function App() {
   const [inputText, setInputText] = useState('#f15025');
   const [error, setError] = useState(false);
   const [list, setList] = useState(new Values('#f15025').all(10));
 
-import Values from 'values.js'
+  const getColor = value => {
+    try {
+      const colors = new Values(value).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
 
   return (
     <>
