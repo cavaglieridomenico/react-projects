c2a5171319684fc8dfa00fd727aafc07e7101714
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-04 16:35:33 +0200

    [ADDED] Component SingleColor Props and Style

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 const SingleColor = ({ weight, type, hex }) => {
+  const hexValue = `#${hex}`;
 
   return (
     <article
+      className={`color ${type === 'shade' ? 'color-light' : false}`}
+      style={{ backgroundColor: `${hexValue}` }}
+      onClick={handleClick}
     >
+      <p className='percent-value'>{weight} %</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
