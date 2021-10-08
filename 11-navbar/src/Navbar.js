4ca2c44192605ffd7a58ad86274263e9b916d4fd
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-08 10:20:05 +0200

    [ADDED] Static structure of App.js

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,18 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <>
+      <div className='nav-header'>
+        <img src={logo} alt='logo' className='logo' />
+      </div>
+      <div className='links-container'></div>
+      <ul className='social-icons'></ul>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
