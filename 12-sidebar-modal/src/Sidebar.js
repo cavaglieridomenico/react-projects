51e32af8839dd6832337b98f6329d2bfe9f926ad
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-11 17:34:43 +0200

    [ADDED] Logic for inserting links

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -12,7 +12,19 @@ const Sidebar = () => {
           <FaTimes />
         </button>
       </div>
-      <ul className='links'>{}</ul>
+      <ul className='links'>
+        {links.map(link => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
       <ul className='social-icons'>{}</ul>
     </aside>
   );
