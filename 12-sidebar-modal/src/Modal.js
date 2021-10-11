ff1a70a72581aee4e765206048f64afb35f0b10c
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-11 17:14:47 +0200

    [ADDED] Modal.js: Imported and used GeneralContext
    Imported and unstructured with useContext and assigned the
    handleShowModal function to the button.

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-const Modal = () => {
-  return <h2>Modal</h2>
-}
+import React, { useState, useContext } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { GeneralContext } from './context';
 
-export default Modal
+const Modal = () => {
+  const { showModal, handleShowModal } = useContext(GeneralContext);
+  return (
+    <div className={`modal-overlay ${showModal && 'show-modal'}`}>
+      <div className='modal-container'>
+        <h3>modal content</h3>
+        <button className='close-modal-btn' onClick={handleShowModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
