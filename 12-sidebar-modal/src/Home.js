cbf8c63a89d1a8f937303b08d47ebe99cc02dbef
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-11 17:11:30 +0200

    [ADDED] Imported and used GeneralContext
    Imported and unstructured with useContext and assigned the
    handleShowModal function to the button.

--- 12-sidebar-modal/src/Home.js
+++ 12-sidebar-modal/src/Home.js
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { GeneralContext } from './context';
 
 const Home = () => {
+  const { handleShowModal } = useContext(GeneralContext);
   return (
     <main>
       <button className='sidebar-toggle'>
         <FaBars />
       </button>
-      <button className='btn'>show modal</button>
+      <button className='btn' onClick={handleShowModal}>
+        show modal
+      </button>
     </main>
   );
 };
