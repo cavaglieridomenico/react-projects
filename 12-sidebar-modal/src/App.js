d4af4c31d5fcc8b0239868e48d5b56a5c3bf67fe
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 08:24:25 +0200

    [UPDATED] Move functions and useState
    Function declarations and useStates related to modal and sidebar have
    been moved to the context.js file.

--- 12-sidebar-modal/src/App.js
+++ 12-sidebar-modal/src/App.js
@@ -1,27 +1,16 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import Modal from './Modal';
 import Sidebar from './Sidebar';
 import Home from './Home';
 import { AppContext } from './context';
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const handleShowModal = () => {
-    setShowModal(!showModal);
-  };
-  const handleShowSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
   return (
-    <AppContext.Provider
-      value={{ showModal, handleShowModal, showSidebar, handleShowSidebar }}
-    >
+    <>
       <Home />
       <Modal />
       <Sidebar />
-    </AppContext.Provider>
+    </>
   );
 }
 
--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,9 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 
 const AppContext = React.createContext();
 
-const AppProvider = () => {
-  return <AppContext.Provider></AppContext.Provider>;
+const AppProvider = ({ children }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+  const handleShowSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ showModal, handleShowModal, showSidebar, handleShowSidebar }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export { AppContext, AppProvider };
