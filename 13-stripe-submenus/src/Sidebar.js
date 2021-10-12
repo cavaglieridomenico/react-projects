4e645bb3ed513355e15f9183354ac8640733b80e
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 15:54:06 +0200

    Static structure of Sidebar.js

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  return (
+    <div className='sidebar-wrapper show'>
+      <aside className='sidebar'>
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'></div>
+      </aside>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
