857804cc7f1b8f37ed7e55e2d1b0fa1ca051d158
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 15:28:11 +0200

    [ADDED] Static structure of Navbar.js

--- 13-stripe-submenus/src/Navbar.js
+++ 13-stripe-submenus/src/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' className='nav' />
+          <button className='btn toggle-btn'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'></ul>
+        <button className='btn'>sign in</button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
