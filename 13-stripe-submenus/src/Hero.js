68a5d2fbffceb17981bb81e5ad2dbebdab6b92b3
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 18:25:48 +0200

    [FIXED] Removed quotes from texts

--- 13-stripe-submenus/src/Hero.js
+++ 13-stripe-submenus/src/Hero.js
@@ -7,14 +7,14 @@ const Hero = () => {
       <div className='hero-center'>
         <article className='hero-info'>
           <h1>
-            "Payments infrastructure "
+            Payments infrastructure
             <br />
-            "for the internet"
+            for the internet
           </h1>
           <p>
-            "Millions of companies of all sizes—from startups to Fortune
-            500s—use Stripe’s software and APIs to accept payments, send
-            payouts, and manage their businesses online."
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
           </p>
           <button className='btn'>start now</button>
         </article>
