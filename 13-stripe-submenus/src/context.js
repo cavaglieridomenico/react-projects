c34b57d3eb2bad65303b597d1d41b51f84d5ede9
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 18:02:08 +0200

    [ADDED] Main structure of context.js
    Created the custom hook useGlobalContext and passed the first prop.

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,14 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value={{ sublinks }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
