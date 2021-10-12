c7ea424d7c5cd2cb0204fb7ac80c9a1920cfcc5c
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-12 16:15:50 +0200

    [ADDED] Logic for sublinks in Sidebar.js
    Used the constant sublinks imported into Sidebar.js

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -4,12 +4,34 @@ import sublinks from './data';
 
 const Sidebar = () => {
   return (
-    <div className='sidebar-wrapper show'>
+    <div className='sidebar-wrapper'>
       <aside className='sidebar'>
         <button className='close-btn'>
           <FaTimes />
         </button>
-        <div className='sidebar-links'></div>
+        <div className='sidebar-links'>
+          <div className='sidebar-links'>
+            {sublinks.map(sublink => {
+              const { page, links } = sublink;
+              return (
+                <article key={new Date().getTime().toString()}>
+                  <h4>{page}</h4>
+                  <div className='sidebar-sublinks'>
+                    {links.map(link => {
+                      const { label, icon, url } = link;
+                      return (
+                        <a href={url} key={new Date().getTime().toString()}>
+                          {icon}
+                          {label}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </article>
+              );
+            })}
+          </div>
+        </div>
       </aside>
     </div>
   );
