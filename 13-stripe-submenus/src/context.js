2cd76842732bc299539bd34077bd45fdc651e9fc
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-13 12:21:45 +0200

    [ADDED] context.js useState for submenu management
    Added useState to check if the submenu is visible, for the data to be
    displayed in the submenu, to define the number of elements (and
    therefore of columns) for each submenu, the html element of the link
    object of the mouse over.

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -5,6 +5,10 @@ const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebar, setIsSidebar] = useState(false);
+  const [isSubmenu, setIsSubmenu] = useState(false);
+  const [submenuItems, setSubmenuItems] = useState(sublinks[0]);
+  const [submenuCol, setSubmenuCol] = useState(0);
+  const [linkElement, setLinkElement] = useState({});
   const openSidebar = () => {
     setIsSidebar(true);
   };
