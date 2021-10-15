5c1ae80287be5236213a2582e56a3e2b9fde9248
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-15 17:53:40 +0200

    [UPDATED] in cartContainer.js state
    it is taken from Context and used to obtain cart data.

--- 14-cart/src/CartContainer.js
+++ 14-cart/src/CartContainer.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from 'react';
+import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
-  if (cart.length === 0) {
+  const { state } = useGlobalContext();
+  console.log(state);
+  console.log(state.cart);
+  if (state.cart.length === 0) {
     return (
       <section className='cart'>
         {/* cart header */}
@@ -13,7 +15,7 @@ const CartContainer = () => {
           <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
-    )
+    );
   }
   return (
     <section className='cart'>
@@ -23,8 +25,8 @@ const CartContainer = () => {
       </header>
       {/* cart items */}
       <div>
-        {cart.map((item) => {
-          return <CartItem key={item.id} {...item} />
+        {state.cart.map(item => {
+          return <CartItem key={item.id} {...item} />;
         })}
       </div>
       {/* cart footer */}
@@ -32,7 +34,7 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            total <span>${state.total}</span>
           </h4>
         </div>
         <button
@@ -43,7 +45,7 @@ const CartContainer = () => {
         </button>
       </footer>
     </section>
-  )
-}
+  );
+};
 
-export default CartContainer
+export default CartContainer;
