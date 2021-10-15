e6348ee737e0b5b17cfec04e6bde6e2293112455
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-15 19:53:24 +0200

    [ADDED] in cartItem.js handleDecrease function
    Decreases the number of items of the same type and if the quantity
    reaches zero, deletes the product line.

--- 14-cart/src/CartItem.js
+++ 14-cart/src/CartItem.js
@@ -29,14 +29,17 @@ const CartItem = ({ amount, id, img, title, price }) => {
         {/* amount */}
         <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className='amount-btn' onClick={() => console.log('decrease')}>
+        <button
+          className='amount-btn'
+          onClick={() => handleDecrease(id, state)}
+        >
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
         </button>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
