e2017e3aec239f145b6e67926f7657e50d7ae3a6
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-21 19:08:24 +0200

    [ADDED] in SearchForm.js main structure and props
    added the render structure and props through the Context.

--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
+  const { loading, setSerchTerm } = useGlobalContext();
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+    <section className='section search'>
+      <form className='search-form'>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input type='text' name='name' id='name' />
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
