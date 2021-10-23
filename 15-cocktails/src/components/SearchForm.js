303d4d3ecd9b48eb03d7e46033a91f093e5c2232
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-23 05:34:59 +0200

    [ADDED] in SearchForm.js useRef and useEffect
    useEffect and useRef assign focus to input to each render of the
    Component. Added preventDefault on submit.

--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  const { loading, setSerchTerm } = useGlobalContext();
+  const { setSearchTerm } = useGlobalContext();
+  const refInput = React.useRef('');
+  React.useEffect(() => {
+    refInput.current.focus();
+  }, []);
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
   return (
     <section className='section search'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
-          <input type='text' name='name' id='name' />
+          <input
+            type='text'
+            name='name'
+            id='name'
+            ref={refInput}
+            onChange={event => setSearchTerm(event.target.value)}
+          />
         </div>
       </form>
     </section>
